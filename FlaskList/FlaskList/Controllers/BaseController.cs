c9934bd6b7ab2118801296a7a29fdce0ff7f6153
Simon Timms <stimms@gmail.com>
2012-09-28 21:29:28 -0600

    Adding base controller

---
+++ FlaskList/FlaskList/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Raven.Client;
+using Raven.Client.Embedded;
+
+namespace FlaskList.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected static readonly IDocumentStore documentStore;
+        protected IDocumentSession _session;
+
+        static BaseController()
+        {
+            documentStore = new EmbeddableDocumentStore 
+            {
+                ConnectionStringName = "RavenDB"
+            }.Initialize();
+        }
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            _session = documentStore.OpenSession();
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            _session.SaveChanges();
+            _session.Dispose();
+        }
+    }
+}
--- FlaskList/FlaskList/Controllers/FlaskController.cs
+++ FlaskList/FlaskList/Controllers/FlaskController.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Linq;
-using Raven.Client;
 using System.Web.Mvc;
 using FlaskList.Models;
-using Raven.Client.Embedded;
 using System.Collections.Generic;
 
 namespace FlaskList.Controllers
 {
-    public class FlaskController : Controller
+    public class FlaskController : BaseController
     {
-
         public ActionResult Index()
         {
             var flasks = _session.Query<Flask>().ToList(); 
@@ -20,7 +17,13 @@ namespace FlaskList.Controllers
         public ActionResult Add(Flask flask)
         {
             _session.Store(flask);
-            return Json(_session.Query<Flask>().Count());
+            return Json(_session.Query<Flask>().Count(),JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult List()
+        {
+            var flasks = _session.Query<Flask>().ToList();
+            return Json(flasks, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult Delete(Guid id)
@@ -28,30 +31,5 @@ namespace FlaskList.Controllers
             _session.Delete(_session.Load<Flask>(id));
             return new EmptyResult();
         }
-
-        #region Database stuff
-        private static readonly IDocumentStore documentStore;
-        private IDocumentSession _session;
-
-        static FlaskController()
-        {
-            documentStore = new EmbeddableDocumentStore 
-            {
-                ConnectionStringName = "RavenDB"
-            }.Initialize();
-        }
-
-        protected override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            _session = documentStore.OpenSession();
-        }
-
-        protected override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            _session.SaveChanges();
-            _session.Dispose();
-        }
-        #endregion
-
     }
 }
